ff80d6941dd5e122a010625642efdf91b912061b
Tayiana <tanyanechay@bk.ru>
2019-04-14 08:04:39 +0300

    Добавлены тесты для сравнения размеров для удаления и модификации контактов (4.5)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupContacts;
 
+import java.util.List;
+
 
 public class ContactDeletionTests extends TestBase {
   public boolean acceptNextAlert = true;
@@ -13,14 +15,14 @@ public class ContactDeletionTests extends TestBase {
     app.getNavigationHelper().gotoGroupContact();
     if (! app.getContactHelper().isThereTheContact()) {
       app.getContactHelper().createContact( new GroupContacts("Name", "Name middle", "Name Last", "Nick", "MyCompany", "My Street", "+79067777777", "email@mail.ru")); }
-    int before = app.getContactHelper().getContactCount();
-    app.getContactHelper().selectContact(before - 1);
-    acceptNextAlert = true;
+    List<GroupContacts> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size() - 1);
+        acceptNextAlert = true;
     app.getContactHelper().deleteContact();
     app.getContactHelper().alertdelete();
     app.getContactHelper().gotoHome();
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before - 1);
+        List<GroupContacts> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() - 1);
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupContacts;
 
+import java.util.List;
+
 public class ContactModificationTests extends TestBase{
 
   @Test
@@ -11,13 +13,14 @@ public class ContactModificationTests extends TestBase{
     app.getNavigationHelper().gotoGroupContact();
     if (! app.getContactHelper().isThereTheContact()) {
       app.getContactHelper().createContact( new GroupContacts("Name", "Name middle", "Name Last", "Nick", "MyCompany", "My Street", "+79067777777", "email@mail.ru")); }
-    int before = app.getContactHelper().getContactCount();
-    app.getContactHelper().selectContact(before - 1);
+    List<GroupContacts> before = app.getContactHelper().getContactList();
+    app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().editContact();
     app.getContactHelper().fillContactForm( new GroupContacts("Name", "Name middle", "Name Last", "Nick", "MyCompany", "My Street", "+79067777777", "email@mail.ru"));
     app.getContactHelper().saveContact();
     app.getContactHelper().gotoHome();
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before );
+    List<GroupContacts> after = app.getContactHelper().getContactList();
+
+    Assert.assertEquals(after.size(), before.size() );
   }
 }
