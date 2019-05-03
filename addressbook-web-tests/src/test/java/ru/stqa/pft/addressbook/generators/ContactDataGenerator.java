7000604bbf06e187cab8f0b5ddd414a9afeb69ec
Tayiana <tanyanechay@bk.ru>
2019-05-04 01:01:54 +0300

    6.5. Загрузка тестовых данных из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -49,8 +49,8 @@ public class ContactDataGenerator {
   private List<GroupContacts> generateGroups(int count) {
     List<GroupContacts> groups = new ArrayList<GroupContacts>();
     for (int i = 0; i < count; i++) {
-      groups.add(new GroupContacts().withFirstname(String.format("firstname %s", i))
-              .withMiddlename(String.format("middlename %s", i)).withLastname(String.format("lastname %s", i)).withNick(String.format("nick %s", i)).withCompany(String.format("company %s", i)).withAddress(String.format("address %s", i)).withHomephone(String.format("phone %s", i)).withMobilphone(String.format("mobilephone %s", i)).withWorkphone(String.format("workphone %s", i)).withEmail(String.format("email %s", i)).withEmail2(String.format("email2 %s", i)).withEmail3(String.format("email3 %s", i)));
+      groups.add(new GroupContacts().withFirstname(String.format("Name %s", i))
+              .withMiddlename(String.format("middlename %s", i)).withLastname(String.format("NameLast %s", i)).withNick(String.format("nick %s", i)).withCompany(String.format("company %s", i)).withAddress(String.format("address %s", i)).withHomephone(String.format("phone %s", i)).withMobilphone(String.format("mobilephone %s", i)).withWorkphone(String.format("workphone %s", i)).withEmail(String.format("email %s", i)).withEmail2(String.format("email2 %s", i)).withEmail3(String.format("email3 %s", i)));
     }
     return groups;
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreationNewContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreationNewContact.java
@@ -4,15 +4,59 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupContacts;
 
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CreationNewContact extends TestBase {
 
-  @Test
-  public void testCreationNewContact()  {
+
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupContacts().withFirstname(split[0]).withMiddlename(split[1]).withLastname(split[2]).withNick(split[3]).withCompany(split[4]).withAddress(split[5]).withHomephone(split[6]).withMobilphone(split[7]).withWorkphone(split[8]).withEmail(split[9]).withEmail2(split[10]).withEmail3(split[11])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test (dataProvider = "validContacts")
+  public void testCreationNewContact(GroupContacts contact)  {
+    app.goTo().GroupContact();
+    Contacts before = app.contact().all();
+    app.contact().create(contact);
+    assertThat(app.contact().getContactCount(), equalTo(before.size()+1));
+    Contacts after = app.contact().all();
+    assertThat(after, equalTo
+            (before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  }
+
+  /*  @Test (dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
+    app.goTo().GroupPage();
+    Groups before = app.group().all();
+    app.group().create(group);
+    assertThat(app.group().count(), equalTo(before.size()+1));
+    Groups after = app.group().all();
+    assertThat(after, equalTo(
+            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  }*/
+
+
+  /*@Test
+  public void testCreationNewContact(GroupContacts contact)  {
     app.goTo().GroupContact();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/picture.jpg");
@@ -22,7 +66,7 @@ public class CreationNewContact extends TestBase {
     Contacts after = app.contact().all();
     assertThat(after, equalTo
             (before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-  }
+  } */
 
   @Test (enabled = false)
   public void CurrentDir(){
